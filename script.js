780cb20b1f98cfa387b2ebadac64c89a415b4aef
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2025-02-03 18:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = 0;
+	for(let i = 0;i<str.length;i++){
+		if(str[i] == str[i+1]){
+			count++;
+		}
+	}
 }
-const input = prompt("Enter a string");
+//const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
